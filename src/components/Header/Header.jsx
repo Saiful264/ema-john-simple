44d2be1ecfb6dc0d01fcdd53b3c9e9aef50e5d12
Saiful264 <saifulislamx264@gmail.com>
2023-04-05 12:41:55 +0600

    all the path and element are set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ function Header() {
         <nav className='header'>
             <img src={logo} alt="" srcset="" />
             <div>
-            <a href="/shop">Shop</a>
-            <a href="/order">Order</a>
+            <a href="/">Shop</a>
+            <a href="/orders">Order</a>
             <a href="/inventory">Inventory</a>
             <a href="/login">Login</a>
             </div>
